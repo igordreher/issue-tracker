167c592bef7d6d6cb8ce86171bad09a37a6ac0fe
igordreher <igord.sparrenberger@gmail.com>
2021-02-09 20:05:43 -0300

    Update migration

--- src/database/migrations/20210209150912_add_issues_columns.ts
+++ src/database/migrations/20210209150912_add_issues_columns.ts
@@ -3,8 +3,15 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.alterTable('issues', table => {
-        table.bigInteger('submitter_id').references('id').inTable('users').notNullable();
-        table.bigInteger('assigned_id').references('id').inTable('users');
+        table.bigInteger('submitter_id').references('id').inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+            .notNullable();
+
+        table.bigInteger('assigned_id').references('id').inTable('users')
+            .onDelete('SET NULL')
+            .onUpdate('CASCADE');
+
         table.dateTime('submit_date').defaultTo(knex.fn.now());
     });
 }
