1a8d7a391771cc333df7954b60b6399edd29d2ad
igordreher <igord.sparrenberger@gmail.com>
2021-02-21 23:22:39 -0300

    Remove unused test dependencies

--- __tests__/test.test.ts
+++
@@ -1,9 +0,0 @@
-import app from 'app';
-import request from 'supertest';
-
-describe('Users', () => {
-    it('should get status 200 from /api/users', (done) => {
-        request(app).get('/api/users')
-            .expect(200, done);
-    });
-});
